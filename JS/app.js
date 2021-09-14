0f56e6110ab41c82d6978a61ef4709d86422985a
SandeshSp <hackersp41@gmail.com>
2021-09-14 21:01:27 +0530

    minor changes

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
 // const themetToggle = document.getElementById('themeChng');
-const slider = document.getElementById('slider');
+const slider = document.getElementById('slider-container');
 const body = document.querySelector('body');
 let theme = 1;
 
@@ -13,18 +13,20 @@ slider.addEventListener('click', () => {
         case 2:
             body.classList.remove('theme-1');
             body.classList.add('theme-2');
+            slider.style.justifyContent = 'center'
             // console.log('theme changed');
             break;
 
         case 3:
             body.classList.remove('theme-2');
             body.classList.add('theme-3');
+            slider.style.justifyContent = 'flex-end';
             // console.log('theme changed');
             break;
         case 4:
             theme = 1;
             body.classList.remove('theme-3');
-            
+            slider.style.justifyContent = 'flex-start';
     }
 }
 
